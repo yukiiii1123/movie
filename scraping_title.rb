8909a1c2470e7e82f3f85a6687a6db8b9f07c628
yukiiii1123 <60319437+yukiiii1123@users.noreply.github.com>
2020-08-05 19:36:07 +0900

    Add files via upload

---
+++ scraping_title.rb
@@ -0,0 +1,7 @@
+require 'mechanize'
+agent= Mechanize.new
+page = agent.get('http://review-movie.herokuapp.com/')
+elements = page.search('.entry-title a')
+elements.each do |ele|
+  puts ele.inner_text
+end
